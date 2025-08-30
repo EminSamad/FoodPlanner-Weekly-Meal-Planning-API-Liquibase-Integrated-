dfac6c795701e9cad5ffdb682684d6a3d43800e4
Emin Samadzade <eminsemedzade7@gmail.com>
2025-08-30 21:54:48 +0400

    Initial commit of the project

---
+++ src/main/java/com/example/devlabtask2/DevLabTask2Application.java
@@ -0,0 +1,13 @@
+package com.example.devlabtask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLabTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevLabTask2Application.class, args);
+    }
+
+}
